ddf9ff2220833ae319502c91b043dea438b3edcd
ayeooong <ayeong0115@naver.com>
2024-09-03 01:30:34 +0900

    Initial commit with React project setup

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), vanillaExtractPlugin()],
+});
